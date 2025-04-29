ea01ce758238d4df34e5b3a1e5d29b5d3ca28adf
Danil <danilekamcuk@gmail.com>
2025-04-29 08:24:46 +0200

    styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: 'public',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  }
 })
